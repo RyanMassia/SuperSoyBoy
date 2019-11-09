0ddb703e02a4c8ccd37b553124b2b044973aeeb2
Ryan Massia [Student] <RMassia03@student.sl.on.ca>
2019-11-09 12:28:02 -0500

    Chapter 12: soyboy can move and jump (high jump needs fixing)

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -12,12 +12,21 @@ public class SoyBoyController : MonoBehaviour
     private SpriteRenderer sr; // sprite renderer
     private Rigidbody2D rb; //rigidbody
     private Animator animator; // finds animator componet
+    public bool isJumping;
+    public float jumpSpeed = 8f;
+    private float rayCastLengthCheck = 0.005f;
+    private float width; 
+    private float height;
+    public float jumpDurationThreshold = 0.25f;
+    private float jumpDuration;
 
     void Awake() // starts up these componets when object is created!
     {
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        width = GetComponent<Collider2D>().bounds.extents.x + 0.1f;
+        height = GetComponent<Collider2D>().bounds.extents.y + 0.2f;
     }
 
      // Start is called before the first frame update
@@ -41,6 +50,25 @@ public class SoyBoyController : MonoBehaviour
         {
             sr.flipX = true;
         }
+
+        if (input.y >= 1f)
+        {
+            jumpDuration += Time.deltaTime;
+        }
+        else
+        {
+            isJumping = false;
+            jumpDuration = 0f;
+        }
+
+        if (PlayerIsOnGround() && isJumping == false)
+        {
+            if (input.y > 0f)
+            {
+                isJumping = true;
+            }
+        }
+        if (jumpDuration > jumpDurationThreshold) input.y = 0f;
     }
 
     void FixedUpdate()
@@ -61,6 +89,27 @@ public class SoyBoyController : MonoBehaviour
         rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x) * acceleration, 0));
         // 4   when not moving velocity is reset to 0
         rb.velocity = new Vector2(xVelocity, rb.velocity.y);
-    } 
+
+        if (isJumping && jumpDuration < jumpDurationThreshold)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
 
     }
+
+    public bool PlayerIsOnGround()
+    {   // 1  
+        bool groundCheck1 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+        bool groundCheck2 = Physics2D.Raycast(new Vector2(transform.position.x + (width - 0.2f),transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+        bool groundCheck3 = Physics2D.Raycast(new Vector2(transform.position.x - (width - 0.2f),transform.position.y - height), -Vector2.up,rayCastLengthCheck);
+        // 2   
+        if (groundCheck1 || groundCheck2 || groundCheck3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
